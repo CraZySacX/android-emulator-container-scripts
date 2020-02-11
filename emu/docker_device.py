50be726a7e39fda463188e85699f5cd4a150c0f1
Jason Ozias <jason.ozias@kroger.com>
2020-02-11 12:45:06 -0500

    docker-prod.kroger.com

--- emu/docker_device.py
+++ emu/docker_device.py
@@ -100,7 +100,7 @@ class DockerDevice(object):
         "FROM nvidia/opengl:1.0-glvnd-runtime-ubuntu18.04 AS emulator\n"
         + "ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES},display"
     )
-    DEFAULT_BASE_IMG = "FROM debian:stretch-slim AS emulator"
+    DEFAULT_BASE_IMG = "FROM docker-prod.registry.kroger.com/debian:stretch-slim AS emulator"
 
     def __init__(self, emulator, sysimg, dest_dir, gpu=False, repo="", tag=""):
 
